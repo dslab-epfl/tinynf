8e90e6ca0ce38c9037c5041fae6b7c261d3b525b
Solal Pirelli <solal.pirelli@gmail.com>
2019-11-12 09:19:48 +0100

    Fix DPDK shim makefiles and statically link libdpdk.
    Click now compiles fine, though at runtime it errors with some burst size.

--- shims/dpdk/mk/rte.vars.mk
+++ shims/dpdk/mk/rte.vars.mk
@@ -1,12 +1,14 @@
+# Click depends on this variable existing
+RTE_SDK_BIN := $(RTE_SDK)/$(RTE_TARGET)
+
 # Define constants such that Click won't try to include any object files whatsoever
-CONFIG_RTE_BUILD_COMBINE_LIBS=y
+# These should be 'y' or 'n'; but for COMBINE_LIBS, we set it to 'x' so that it's completely ignored
+# and thus Click will neither include libs individually nor include the overall lib
+CONFIG_RTE_BUILD_COMBINE_LIBS=x
 CONFIG_RTE_LIBRTE_PMD_PCAP=n
 CONFIG_RTE_LIBRTE_PMD_SZEDATA2=n
 CONFIG_RTE_LIBRTE_VHOST_NUMA=n
 CONFIG_RTE_LIBRTE_VHOST_USER=y
-
-# We need libnuma, this convenient variable is added to the linker options
-EXTRA_LDLIBS += -lnuma
 
 # Generate the "DPDK" library, a.k.a. our stub with global variables
 TN_DPDK_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))/..
@@ -14,5 +16,6 @@ TN_DIR := $(TN_DPDK_DIR)/../../code
 TN_FILES := $(shell echo $(TN_DPDK_DIR)/tn_dpdk.c $(TN_DIR)/util/*.c $(TN_DIR)/env/linux-x86/*.c $(TN_DIR)/net/82599/*.c)
 _IGNORED := $(shell mkdir -p $(TN_DPDK_DIR)/lib; \
               g++ -shared -o $(TN_DPDK_DIR)/lib/libdpdk.so -std=c++17 -D_GNU_SOURCE -fPIC -I $(TN_DPDK_DIR)/include $(TN_FILES))
-#              g++ -shared $(TN_DPDK_DIR)/mk/*.o -o $(TN_DPDK_DIR)/lib/libdpdk.so)
-RTE_LIBNAME=dpdk
+
+# Statically link our libdpdk so we don't have to mess with LD_LIBRARY_PATH (and don't forget libnuma!)
+EXTRA_LDLIBS += $(TN_DPDK_DIR)/lib/libdpdk.so -lnuma
