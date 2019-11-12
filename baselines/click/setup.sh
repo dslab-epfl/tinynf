ab2447821da693f3f7b490dd8bde0044eb9bd3aa
Solal Pirelli <solal.pirelli@gmail.com>
2019-11-12 13:25:55 +0100

    Allow overrides and cflags in Click makefile

--- baselines/click/setup.sh
+++ baselines/click/setup.sh
@@ -3,9 +3,13 @@ export RTE_TARGET=.
 export TN_CC=g++
 
 cd click
+
 if [ ! -f 'Makefile' ]; then
   ./configure --enable-dpdk --enable-user-multithread
 fi
-if [ ! -f 'bin/click' ]; then
+
+if [ ! -f '.cflags' ] || [ "$(cat .cflags)" != "$TN_CFLAGS" ]; then
+  make clean
   make
+  echo "$TN_CFLAGS" > '.cflags'
 fi
