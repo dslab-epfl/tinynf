3561cb36ad0dd123bffd017e3e3b88012d212f3c
Solal Pirelli <solal.pirelli@gmail.com>
2019-11-20 10:52:05 +0100

    Fix makefile for DPDK baseline
    We need to handle our own DPDK shim separately since it doesn't need all the device-binding stuff
    Also make DPDK every time and rm/insmod the module just in case; DPDK tends to randomly break otherwise

--- shims/dpdk/mk/rte.extapp.mk
+++ shims/dpdk/mk/rte.extapp.mk
@@ -2,7 +2,7 @@
 # Projects expect an 'all' target that compiles the $(SRCS-y) files using $(CC) $(CFLAGS) and $(LD) $(LDFLAGS) into $(PWD)/build/app/$(APP), linking with DPDK
 
 all:
-	rm -rf "$(PWD)/build"
-	mkdir -p "$(PWD)/build/app"
-	for file in $(SRCS-y); do $(CC) -c "$$file" $(CFLAGS) -I "$(TN_DPDK_DIR)/include" -o "$(PWD)/build/$$(basename $$file).o"; done
-	$(LD) "$(PWD)/build/"*.o "$(TN_DPDK_DIR)/lib/libdpdk.so" $(LDFLAGS) $(TN_LDFLAGS) -o "$(PWD)/build/app/$(APP)"
+	@rm -rf "$(PWD)/build"
+	@mkdir -p "$(PWD)/build/app"
+	@for file in $(SRCS-y); do $(CC) -c "$$file" $(CFLAGS) -I "$(TN_DPDK_DIR)/include" -o "$(PWD)/build/$$(basename $$file).o"; done
+	@$(LD) "$(PWD)/build/"*.o "$(TN_DPDK_DIR)/lib/libdpdk.so" $(LDFLAGS) $(TN_LDFLAGS) -o "$(PWD)/build/app/$(APP)"
