0be69e218749c7d42b5d22268429b46e05947b06
Solal Pirelli <solal.pirelli@gmail.com>
2019-11-29 09:40:46 +0100

    Remove unneeded code in tinynf.c

--- code/tinynf.c
+++ code/tinynf.c
@@ -1,50 +1,29 @@
-#include "env/memory.h"
 #include "env/pci.h"
 #include "net/network.h"
 #include "util/log.h"
 #include "util/parse.h"
 
-
-#define DEVICES_MAX_COUNT 128u
+// This NF does as little as possible, it's only intended for benchmarking/profiling the driver
 
 static uint16_t tinynf_packet_handler(uint8_t* packet, uint16_t packet_length, bool* send_list)
 {
-	// SRC MAC
-	packet[0] = 0x00;
-	packet[1] = 0x01;
-	packet[2] = 0x02;
-	packet[3] = 0x03;
-	packet[4] = 0x04;
-	packet[5] = 0x05;
-	// DST MAC
-	packet[6] = 0xFF;
-	packet[7] = 0xFF;
-	packet[8] = 0xFF;
-	packet[9] = 0xFF;
-	packet[10]= 0xFF;
-	packet[11]= 0xFF;
+	(void) packet;
 
 	send_list[0] = true;
-
 	return packet_length;
 }
 
 int main(int argc, char** argv)
 {
 	uint64_t devices_count = (uint64_t) argc - 1;
-	struct tn_pci_device pci_devices[DEVICES_MAX_COUNT];
-	if (devices_count == 0 || devices_count > DEVICES_MAX_COUNT || !tn_util_parse_pci(devices_count, argv+1, pci_devices)) {
-		TN_INFO("Couldn't parse PCI devices from argv");
+	struct tn_pci_device pci_devices[2];
+	if (devices_count != 2 || !tn_util_parse_pci(devices_count, argv+1, pci_devices)) {
+		TN_INFO("Couldn't parse two PCI devices from argv");
 		return 1;
 	}
 
-	if (devices_count != 2) {
-		TN_INFO("Sorry, this simple NF is just for bench purposes so it supports 2 devices exactly");
-		return 1;
-	}
-
-	struct tn_net_pipe* pipes[DEVICES_MAX_COUNT];
-	struct tn_net_device* devices[DEVICES_MAX_COUNT];
+	struct tn_net_pipe* pipes[2];
+	struct tn_net_device* devices[2];
 	for (uint8_t n = 0; n < devices_count; n++) {
 		if (!tn_net_pipe_init(&(pipes[n]))) {
 			TN_INFO("Couldn't init pipe");
