82dce1258b57d48890d522684d5299648b7acede
Solal Pirelli <solal.pirelli@gmail.com>
2020-01-20 15:46:00 +0100

    Switch to gcc-9 (less instrs/l1i misses), count cycles with papi

--- code/util/perf.h
+++ code/util/perf.h
@@ -21,7 +21,7 @@ static struct perf_measurement perf_measurements[10 * 1024 * 1024];
 #define TN_PERF_CYCLES_DUMP() do { for (uint64_t n = 1; n < perf_index; n++) { printf("%s: %"PRIu64"\n", perf_measurements[n].name, perf_measurements[n].tsc - perf_measurements[n-1].tsc); } fflush(stdout); } while(0)
 
 
-static int papi_events[] = {PAPI_TOT_INS, PAPI_L1_DCM, PAPI_L1_ICM, PAPI_L2_TCM, PAPI_L3_TCM};
+static int papi_events[] = {PAPI_TOT_CYC, PAPI_TOT_INS, PAPI_L1_DCM, PAPI_L1_ICM, PAPI_L2_TCM, PAPI_L3_TCM};
 static uint64_t papi_counter;
 #define papi_events_count sizeof(papi_events)/sizeof(papi_events[0])
 #define TN_PERF_PAPI_BATCH_SIZE 10000
@@ -29,7 +29,7 @@ static long long papi_values[TN_PERF_PAPI_BATCH_SIZE][papi_events_count]; \
 
 // PAPI functions: call START(), then RESET() before your event and RECORD() immediately after
 #define TN_PERF_PAPI_START() do { \
-		printf("Counters: instructions, L1d, L1i, L2, L3\n"); \
+		printf("Counters: cycles, instructions, L1d, L1i, L2, L3\n"); \
 		if (PAPI_start_counters(papi_events, papi_events_count) != PAPI_OK) { \
 			printf("Couldn't start PAPI counters!\n"); \
 			exit(1); \
