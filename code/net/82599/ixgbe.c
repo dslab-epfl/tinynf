7db2e955bd793b153bddc2bc657be53d82acd9d3
Solal Pirelli <solal.pirelli@gmail.com>
2020-05-25 21:43:28 +0000

    Update VIGOR_SYMBEX code after latest scheduling changes

--- code/net/82599/ixgbe.c
+++ code/net/82599/ixgbe.c
@@ -1191,7 +1191,8 @@ bool tn_net_agent_receive(struct tn_net_agent* agent, uint8_t** out_packet, uint
 #ifdef VIGOR_SYMBEX
 	// Not great; but less assumptions than Vigor makes in the DPDK driver patches
 	agent->processed_delimiter = 0;
-	agent->flushed_processed_delimiter = (uint64_t) -1;
+	agent->flush_counter = 0;
+	agent->flush_target = 0;
 #endif
 
 	// Since descriptors are 16 bytes, the index must be doubled
