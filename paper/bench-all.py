7c67e1ad0e892a6122d52c531f093835205236e4
Solal Pirelli <solal.pirelli@gmail.com>
2020-08-18 13:14:24 +0200

    Remove TN_STRIP, pass -s to gcc in bench-all instead

--- paper/bench-all.py
+++ paper/bench-all.py
@@ -82,9 +82,9 @@ def get_benchflags(nf, env):
 def get_cflags(nf, env):
   result = []
   if not has_dpdk(env) or env['RTE_TARGET'] == '.':
-    result.append('-flto')
-    result.append('-DIXGBE_AGENT_OUTPUTS_MAX=1')
-    result.append('-DASSUME_ONE_WAY')
+    result.append('-flto') # LTO
+    result.append('-s') # strip
+    result.append('-DASSUME_ONE_WAY') # no unnecessary agents
   return result
 
 def get_layer(nf, env):
@@ -105,7 +105,6 @@ def bench_core(nf, nf_dir, benchflags, additional_env):
   env.update(get_env(nf, env))
   env.update(additional_env)
   env['TN_CFLAGS'] = " ".join(get_cflags(nf, env))
-  env['TN_LDFLAGS'] = env['TN_CFLAGS'] # TODO this should not be needed, fix makefiles
 
   benchflags = get_benchflags(nf, env) + benchflags + [str(get_layer(nf, env))]
 
@@ -132,16 +131,13 @@ def bench(path, nf, kind, env):
   bench_core(nf, THIS_DIR + '/../' + path, ['standard'], env)
   remove(out_folder + '/latencies') # don't keep old latencies around
   move_into(BENCH_RESULTS_PATH, out_folder)
-  #bench_core(nf, THIS_DIR + '/../' + path, ['--acceptableloss=0', '--latencyload=-1', 'standard'], env)
-  #add_suffix(BENCH_RESULT_TPUT_PATH, '-zeroloss')
-  #move_into(BENCH_RESULT_TPUT_PATH + '-zeroloss', out_folder)
 
 
 # Overall, because binding DPDK's igb_uio driver has a slight chance of hanging the machine for some reason,
 # each step performs all non-DPDK stuff first then all DPDK stuff
 
 # DPDK's testpmd no-op, batched or not, vs TinyNF no-op vs Ixy no-op (throughput, zero-loss throughput, detailed latency)
-if 0:
+if 1:
   bench('code', 'nop', 'tinynf', {})
   bench('baselines/ixy', 'nop', 'ixy', {})
   bench('baselines/ixy', 'nop', 'ixy', {'TN_BATCH_SIZE': BATCH_SIZE})
@@ -159,19 +155,22 @@ if 1:
   bench('baselines/parallel-policer/dpdk', 'pol', 'dpdk-parallel', {'RTE_SDK': RTE_SDK, 'RTE_TARGET': RTE_TARGET, 'TN_BATCH_SIZE': BATCH_SIZE})
 
 # Vigor NFs, as well as batched NAT for latency
-if 0:
+if 1:
   for nf in ['nat', 'bridge', 'fw', 'pol', 'lb']:
     bench_vigor(nf, {})
   for nf in ['nat', 'bridge', 'fw', 'pol', 'lb']:
     bench_vigor(nf, {'RTE_SDK': RTE_SDK, 'RTE_TARGET': RTE_TARGET})
   bench_vigor('nat', {'RTE_SDK': RTE_SDK, 'RTE_TARGET': RTE_TARGET, 'TN_BATCH_SIZE': BATCH_SIZE})
 
-# Click no-op with TinyNF vs DPDK vs DPDK batch
+
+
+
+# OLD: Click no-op with TinyNF vs DPDK vs DPDK batch
 if 0:
   bench('baselines/click', 'nop', 'click', {})
   bench('baselines/click/with-dpdk', 'nop', 'click', {'RTE_SDK': RTE_SDK, 'RTE_TARGET': RTE_TARGET})
   bench('baselines/click/with-dpdk', 'nop', 'click', {'RTE_SDK': RTE_SDK, 'RTE_TARGET': RTE_TARGET, 'TN_BATCH_SIZE': BATCH_SIZE})
 
-# DPDK l3fwd, which should reach 2x10 Gb/s line rate, as indicated in the DPDK perf reports
+# SANITY CHECK: DPDK l3fwd, which should reach 2x10 Gb/s line rate, as indicated in the DPDK perf reports
 if 0:
   bench('baselines/dpdk/l3fwd', 'l3fwd', 'dpdk', {'RTE_SDK': RTE_SDK, 'RTE_TARGET': RTE_TARGET})
