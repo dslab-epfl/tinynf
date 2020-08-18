21e65ac5cacb36b49e16d8dce00617f105e8b22f
Solal Pirelli <solal.pirelli@gmail.com>
2020-08-18 15:40:04 +0200

    Use pregenerated Vigor baseline sources

--- baselines/vigor/setup.sh
+++ baselines/vigor/setup.sh
@@ -2,20 +2,10 @@
 
 git submodule update --init --recursive
 
-# Simplified version of the vigor setup script, with only what we need to run Vigor on top of our code
-
-sudo apt-get install -y opam m4 python3.6
-opam init -y
-opam switch 4.06.0
-opam install goblint-cil core ocamlfind -y
-
-if ! grep -q opam "$HOME/.profile"; then
-  echo 'PATH='"$HOME/.opam/system/bin"':$PATH' >> "$HOME/.profile"
-  echo ". $HOME/.opam/opam-init/init.sh" >> "$HOME/.profile"
-  . "$HOME/.profile"
-fi
+# Simplified version of the vigor setup script, we use the pregenerated branch so nothing else is needed
 
 cd dpdk
+
 for p in ../vigor/setup/dpdk.*.patch; do
   patch -p1 < "$p"
 done
