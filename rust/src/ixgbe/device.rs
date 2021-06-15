1c5f6bdda2f0d4db9386e00212291dc918ea4cf0
Solal Pirelli <solal.pirelli@gmail.com>
2021-06-15 17:07:23 +0200

    Finish porting Device

--- rust/src/ixgbe/device.rs
+++ rust/src/ixgbe/device.rs
@@ -234,92 +234,48 @@ impl Device<'_> {
 
         &mut self.buffer[regs::RDT(queue_index) as usize / size_of::<u32>()]
     }
-/*
-        // -------------------------------------
-        // Section 4.6.8 Transmit Initialization
-        // -------------------------------------
-        internal Memory<uint> AddOutput(IEnvironment env, Span<Descriptor> ring, ref uint transmitHead)
-        {
-        uint queue_index = 0;
-        for (; queue_index < DeviceLimits.TransmitQueuesCount; queue_index++)
-        {
-            // See later for details of TXDCTL.ENABLE
-            if (regs::is_field_cleared(buffer, regs::TXDCTL(queue_index), regs::TXDCTL_::ENABLE))
-            {
+
+    pub fn add_output<'a>(&'a mut self, env: &impl Environment, ring: &mut [Descriptor], transmit_head: &mut u32) -> &'a mut u32 {
+        let mut queue_index: u32 = 0;
+        while queue_index < device_limits::TRANSMIT_QUEUES_COUNT {
+            if regs::is_field_cleared(self.buffer, regs::TXDCTL(queue_index), regs::TXDCTL_::ENABLE) {
                 break;
             }
+            queue_index = queue_index + 1;
         }
-        if (queue_index == DeviceLimits.TransmitQueuesCount)
-        {
+        if queue_index == device_limits::TRANSMIT_QUEUES_COUNT {
             panic!("No available transmit queues");
         }
 
-        // "The following steps should be done once per transmit queue:"
-        // "- Allocate a region of memory for the transmit descriptor list."
-        // This is already done in agent initialization as agent->rings[*]
+        let ring_phys_addr = env.get_physical_address(ring);
+        regs::write(self.buffer, regs::TDBAH(queue_index), (ring_phys_addr >> 32) as u32);
+        regs::write(self.buffer, regs::TDBAL(queue_index), ring_phys_addr as u32);
 
-        // "- Program the descriptor base address with the address of the region (TDBAL, TDBAH)."
-        // 	Section 8.2.3.9.5 Transmit Descriptor Base Address Low (TDBAL[n]):
-        // 	"The Transmit Descriptor Base Address must point to a 128 byte-aligned block of data."
-        // This alignment is guaranteed by the agent initialization
-        nuint ring_phys_addr = env.GetPhysicalAddress(ref ring.GetPinnableReference());
-        regs::write(buffer, regs::TDBAH(queue_index), (uint)(ring_phys_addr >> 32));
-        regs::write(buffer, regs::TDBAL(queue_index), (uint)ring_phys_addr);
-        // "- Set the length register to the size of the descriptor ring (TDLEN)."
-        // 	Section 8.2.3.9.7 Transmit Descriptor Length (TDLEN[n]):
-        // 	"This register sets the number of bytes allocated for descriptors in the circular descriptor buffer."
-        // Note that each descriptor is 16 bytes.
-        regs::write(buffer, regs::TDLEN(queue_index), DriverConstants.RingSize * 16u);
-        // "- Program the TXDCTL register with the desired TX descriptor write back policy (see Section 8.2.3.9.10 for recommended values)."
-        //	Section 8.2.3.9.10 Transmit Descriptor Control (TXDCTL[n]):
-        //	"HTHRESH should be given a non-zero value each time PTHRESH is used."
-        //	"For PTHRESH and HTHRESH recommended setting please refer to Section 7.2.3.4."
-        // INTERPRETATION-MISSING: The "recommended values" are in 7.2.3.4.1 very vague; we use (cache line size)/(descriptor size) for HTHRESH (i.e. 64/16 by assumption CACHE),
-        //                 and a completely arbitrary value for PTHRESH
-        // PERFORMANCE: This is required to forward 10G traffic on a single NIC.
-        regs::write_field(buffer, regs::TXDCTL(queue_index), regs::TXDCTL_::PTHRESH, 60);
-        regs::write_field(buffer, regs::TXDCTL(queue_index), regs::TXDCTL_::HTHRESH, 4);
-        // "- If needed, set TDWBAL/TWDBAH to enable head write back."
-        nuint headPhysAddr = env.GetPhysicalAddress(ref transmitHead);
-        //	Section 7.2.3.5.2 Tx Head Pointer Write Back:
-        //	"The low register's LSB hold the control bits.
-        // 	 * The Head_WB_EN bit enables activation of tail write back. In this case, no descriptor write back is executed.
-        // 	 * The 30 upper bits of this register hold the lowest 32 bits of the head write-back address, assuming that the two last bits are zero."
-        //	"software should [...] make sure the TDBAL value is Dword-aligned."
-        //	Section 8.2.3.9.11 Tx Descriptor completion Write Back Address Low (TDWBAL[n]): "the actual address is Qword aligned"
-        // INTERPRETATION-CONTRADICTION: There is an obvious contradiction here; qword-aligned seems like a safe option since it will also be dword-aligned.
-        // INTERPRETATION-INCORRECT: Empirically, the answer is... 16 bytes. Write-back has no effect otherwise. So both versions are wrong.
-        if (headPhysAddr % 16u != 0)
-        {
+        regs::write(self.buffer, regs::TDLEN(queue_index), driver_constants::RING_SIZE * 16);
+
+        regs::write_field(self.buffer, regs::TXDCTL(queue_index), regs::TXDCTL_::PTHRESH, 60);
+        regs::write_field(self.buffer, regs::TXDCTL(queue_index), regs::TXDCTL_::HTHRESH, 4);
+
+        let head_phys_addr = env.get_physical_address(transmit_head);
+        if head_phys_addr % 16 != 0 {
             panic!("Transmit head's physical address is not aligned properly");
         }
-        //	Section 8.2.3.9.11 Tx Descriptor Completion Write Back Address Low (TDWBAL[n]):
-        //	"Head_WB_En, bit 0 [...] 1b = Head write-back is enabled."
-        //	"Reserved, bit 1"
-        regs::write(buffer, regs::TDWBAH(queue_index), (uint)(headPhysAddr >> 32));
-        regs::write(buffer, regs::TDWBAL(queue_index), (uint)headPhysAddr | 1u);
-        // INTERPRETATION-MISSING: We must disable relaxed ordering of head pointer write-back, since it could cause the head pointer to be updated backwards
-        regs::clear_field(buffer, regs::DCATXCTRL(queue_index), regs::DCATXCTRL_::TX_DESC_WB_RO_EN);
-        // "- Enable transmit path by setting DMATXCTL.TE.
-        //    This step should be executed only for the first enabled transmit queue and does not need to be repeated for any following queues."
-        if (!_txEnabled)
-        {
-            regs::set_field(buffer, regs::DMATXCTL, regs::DMATXCTL_::TE);
-            _txEnabled = true;
+
+        regs::write(self.buffer, regs::TDWBAH(queue_index), (head_phys_addr >> 32) as u32);
+        regs::write(self.buffer, regs::TDWBAL(queue_index), head_phys_addr as u32 | 1);
+
+        regs::clear_field(self.buffer, regs::DCATXCTRL(queue_index), regs::DCATXCTRL_::TX_DESC_WB_RO_EN);
+
+        if !self.tx_enabled {
+            regs::set_field(self.buffer, regs::DMATXCTL, regs::DMATXCTL_::TE);
+            self.tx_enabled = true;
         }
-        // "- Enable the queue using TXDCTL.ENABLE.
-        //    Poll the TXDCTL register until the Enable bit is set."
-        // INTERPRETATION-MISSING: No timeout is mentioned here, let's say 1s to be safe.
-        regs::set_field(buffer, regs::TXDCTL(queue_index), regs::TXDCTL_::ENABLE);
-        if_after_timeout(env, Duration::from_secs(1), ???, || regs::is_field_cleared(buffer, regs::TXDCTL(queue_index), regs::TXDCTL_::ENABLE), ||
-        {
+
+        regs::set_field(self.buffer, regs::TXDCTL(queue_index), regs::TXDCTL_::ENABLE);
+        if_after_timeout(env, Duration::from_secs(1), self.buffer, |b| regs::is_field_cleared(b, regs::TXDCTL(queue_index), regs::TXDCTL_::ENABLE), |_| {
             panic!("TXDCTL.ENABLE did not set, cannot enable queue");
         });
-        // "Note: The tail register of the queue (TDT) should not be bumped until the queue is enabled."
-        // Nothing to transmit yet, so leave TDT alone.
 
-        return buffer.Slice((int)regs::TDT(queue_index) / sizeof(uint), 1);
-        }
+        &mut self.buffer[regs::TDT(queue_index) as usize / size_of::<u32>()]
     }
-*/
 }
