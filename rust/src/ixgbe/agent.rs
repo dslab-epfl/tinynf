a532bfe3e5dbd7d69fa28848a3a9e2ad5f828387
Solal Pirelli <solal.pirelli@gmail.com>
2021-06-15 20:06:02 +0200

    WIP more agent work

--- rust/src/ixgbe/agent.rs
+++ rust/src/ixgbe/agent.rs
@@ -11,41 +11,49 @@ use super::device::Device;
 
 pub struct Agent<'a> {
     packets: &'a mut [[u8; PACKET_SIZE]; driver_constants::RING_SIZE],
-    receive_ring: &'a [Descriptor; driver_constants::RING_SIZE],
+    first_ring: &'a mut [Descriptor; driver_constants::RING_SIZE],
     receive_tail: &'a mut u32,
-    transmit_rings: &'a [&'a mut [Descriptor; driver_constants::RING_SIZE]],
+    other_rings: Vec<&'a mut [Descriptor; driver_constants::RING_SIZE]>,
     transmit_heads: &'a [TransmitHead],
-    transmit_tails: &'a mut [&'a mut u32], // the slice itself must be mutable, which is weird because conceptually it isn't
+    transmit_tails: Vec<&'a mut u32>,
     outputs: &'a mut [u16; driver_constants::MAX_OUTPUTS],
     process_delimiter: u8
 }
 
 impl Agent<'_> {
-    pub fn create<'a>(env: &impl Environment, input_device: &'a mut Device, output_devices: &[&'a mut Device]) -> Agent<'a> {
+    pub fn create<'a>(env: &'a impl Environment, input_device: &'a mut Device, output_devices: &'a mut [&'a mut Device]) -> Agent<'a> {
         if output_devices.len() > driver_constants::MAX_OUTPUTS {
             panic!("Too many outputs");
         }
     
         let packets = env.allocate::<[u8; PACKET_SIZE], { driver_constants::RING_SIZE }>();
 
-        let rings: &'a [&'a mut [Descriptor; driver_constants::RING_SIZE]] = output_devices.iter().map(|_| env.allocate::<Descriptor, { driver_constants::RING_SIZE }>()).collect();
-        for ring in rings {
+        let first_ring = env.allocate::<Descriptor, { driver_constants::RING_SIZE }>();
+        for n in 0..driver_constants::RING_SIZE {
+            first_ring[n as usize].buffer = u64::to_le(env.get_physical_address(&mut packets[n as usize]) as u64);
+        }
+        let mut other_rings: Vec<&'a mut [Descriptor; driver_constants::RING_SIZE]> = (0..output_devices.len()-1).map(|_| env.allocate::<Descriptor, { driver_constants::RING_SIZE }>()).collect();
+        for ring in other_rings.iter_mut() {
             for n in 0..driver_constants::RING_SIZE {
-                ring[n as usize].buffer = u64::to_le(env.get_physical_address(&mut packets[n as usize]) as u64);
+                ring[n as usize].buffer = first_ring[n as usize].buffer;
             }
         }
         
-        let receive_tail = input_device.set_input(env, &mut rings[0]);
+        let receive_tail = input_device.set_input(env, first_ring);
 
         let transmit_heads = env.allocate_slice::<TransmitHead>(output_devices.len());
 
-        let transmit_tails = output_devices.iter().zip(rings.iter_mut()).zip(transmit_heads.iter_mut()).map(|((&d, &mut r), &mut h)| d.add_output(env, &mut r, &mut h.value)).collect();
+        let mut transmit_tails = Vec::new();
+        transmit_tails.push(output_devices[0].add_output(env, first_ring, &mut transmit_heads[0].value));
+        for n in 1..transmit_heads.len() {
+            transmit_tails.push(output_devices[n].add_output(env, other_rings[n-1], &mut transmit_heads[n].value));
+        }
 
         Agent {
             packets,
-            receive_ring: &rings[0],
+            first_ring,
             receive_tail,
-            transmit_rings: rings,
+            other_rings,
             transmit_heads,
             transmit_tails,
             outputs: env.allocate::<u16, { MAX_OUTPUTS }>(),
@@ -60,7 +68,7 @@ impl Agent<'_> {
                 break true;
             }
             
-            let receive_metadata = u64::from_le(volatile::read(&self.receive_ring[self.process_delimiter as usize].metadata));
+            let receive_metadata = u64::from_le(volatile::read(&self.first_ring[self.process_delimiter as usize].metadata));
             if (receive_metadata & (1 << 32)) == 0 {
                 break n != 0;
             }
@@ -69,9 +77,11 @@ impl Agent<'_> {
             processor(&mut self.packets[self.process_delimiter as usize], length, self.outputs);
             
             let rs_bit: u64 = if self.process_delimiter % driver_constants::FLUSH_PERIOD == (driver_constants::FLUSH_PERIOD - 1) { 1 << 24+3 } else { 0 };
-            for n in 0..self.transmit_rings.len() {
-                volatile::write(&mut self.transmit_rings[n as usize][self.process_delimiter as usize].metadata, u64::to_le(self.outputs[n as u8 as usize] as u64 | (1 << 24+1) | (1 << 24) | rs_bit));
-                self.outputs[n as u8 as usize] = 0;
+            volatile::write(&mut self.first_ring[self.process_delimiter as usize].metadata, u64::to_le(self.outputs[0] as u64 | (1 << 24+1) | (1 << 24) | rs_bit));
+            self.outputs[0] = 0;
+            for n in 0..self.other_rings.len() {
+                volatile::write(&mut self.other_rings[n as usize][self.process_delimiter as usize].metadata, u64::to_le(self.outputs[(n + 1) as u8 as usize] as u64 | (1 << 24+1) | (1 << 24) | rs_bit));
+                self.outputs[(n + 1) as u8 as usize] = 0;
             }
 
             if rs_bit != 0 {
@@ -86,13 +96,13 @@ impl Agent<'_> {
                     }
                 }
                 
-                volatile::write(&mut self.receive_tail, u32::to_le((earliest_transmit_head - 1) % driver_constants::RING_SIZE as u32));
+                volatile::write::<u32>(&mut self.receive_tail, u32::to_le((earliest_transmit_head - 1) % driver_constants::RING_SIZE as u32));
             }
             n = n + 1;
         };
         if needs_flushing {
-            for &mut tail in self.transmit_tails {
-                volatile::write(&mut tail, u32::to_le(self.process_delimiter as u32));
+            for tail in self.transmit_tails.iter_mut() {
+                volatile::write::<u32>(tail, u32::to_le(self.process_delimiter as u32));
             }
         }
     }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,7 +6,7 @@ mod ixgbe;
 use ixgbe::agent::Agent;
 use ixgbe::device::Device;
 
-fn proc(data: &mut [u8; ixgbe::PACKET_SIZE], length: u16, output_lengths: [u16; ixgbe::MAX_OUTPUTS]) {
+fn proc(data: &mut [u8; ixgbe::PACKET_SIZE], length: u16, output_lengths: &mut [u16; ixgbe::MAX_OUTPUTS]) {
     if data[0] == 42 {
         output_lengths[0] = length;
     }
