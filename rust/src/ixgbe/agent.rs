036568340d61533fd4c1c60e725b2b0c611b6fea
Solal Pirelli <solal.pirelli@gmail.com>
2022-02-21 11:52:22 +0100

    Rust: Use non_empty_vec crate to simplify non-const agent

--- rust/src/ixgbe/agent.rs
+++ rust/src/ixgbe/agent.rs
@@ -1,3 +1,5 @@
+use non_empty_vec::NonEmpty; // this crate uses unsafe but then Vec itself uses unsafe so...
+
 use crate::env::Environment;
 use crate::volatile;
 
@@ -10,9 +12,8 @@ const RECYCLE_PERIOD: u8 = 64;
 
 pub struct Agent<'a, 'b> {
     packets: &'b mut [PacketData; RING_SIZE],
-    first_ring: &'b mut [Descriptor; RING_SIZE],
+    rings: NonEmpty<&'b mut [Descriptor; RING_SIZE]>,
     receive_tail: &'a mut u32,
-    other_rings: Vec<&'b mut [Descriptor; RING_SIZE]>,
     transmit_heads: &'b [TransmitHead],
     transmit_tails: Vec<&'a mut u32>,
     outputs: &'b mut [u16; MAX_OUTPUTS],
@@ -27,36 +28,32 @@ impl Agent<'_, '_> {
 
         let packets = env.allocate::<PacketData, { RING_SIZE }>();
 
-        let first_ring = env.allocate::<Descriptor, { RING_SIZE }>();
+        let mut rings = NonEmpty::new(env.allocate::<Descriptor, { RING_SIZE }>());
         for n in 0..RING_SIZE {
-            first_ring[n as usize].buffer = u64::to_le(env.get_physical_address(&mut packets[n as usize]) as u64);
+            rings[0][n as usize].buffer = u64::to_le(env.get_physical_address(&mut packets[n as usize]) as u64);
         }
-
-        let mut other_rings = Vec::new();
         for _n in 0..outputs.len() - 1 {
-            other_rings.push(env.allocate::<Descriptor, { RING_SIZE }>());
-        }
-        for ring in other_rings.iter_mut() {
+            let r = env.allocate::<Descriptor, { RING_SIZE }>();
             for n in 0..RING_SIZE {
-                ring[n as usize].buffer = first_ring[n as usize].buffer;
+                r[n as usize].buffer = rings[0][n as usize].buffer;
             }
+            rings.push(r);
         }
 
-        let receive_tail = input.associate(env, first_ring);
+        let receive_tail = input.associate(env, rings[0]);
 
         let transmit_heads = env.allocate_slice::<TransmitHead>(outputs.len());
 
         let mut transmit_tails = Vec::new();
         for out in outputs.iter_mut() {
             let n = transmit_tails.len();
-            transmit_tails.push(out.associate(env, if n == 0 { first_ring } else { other_rings[n - 1] }, &mut transmit_heads[n].value));
+            transmit_tails.push(out.associate(env, rings[n], &mut transmit_heads[n].value));
         }
 
         Agent {
             packets,
-            first_ring,
+            rings,
             receive_tail,
-            other_rings,
             transmit_heads,
             transmit_tails,
             outputs: env.allocate::<u16, { MAX_OUTPUTS }>(),
@@ -68,7 +65,7 @@ impl Agent<'_, '_> {
     pub fn run(&mut self, processor: fn(&mut PacketData, u16, &mut [u16; MAX_OUTPUTS])) {
         let mut n: u8 = 0;
         while n < FLUSH_PERIOD {
-            let receive_metadata = u64::from_le(volatile::read(&self.first_ring[self.process_delimiter as usize].metadata));
+            let receive_metadata = u64::from_le(volatile::read(&self.rings.first()[self.process_delimiter as usize].metadata));
             if (receive_metadata & (1 << 32)) == 0 {
                 break;
             }
@@ -81,16 +78,8 @@ impl Agent<'_, '_> {
             } else {
                 0
             };
-            // This is rather awkward, we can't have transmit_rings[0] as the "main" ring because then we'd incur a bounds check when using it for RX,
-            // but we also can't have a reference to transmit_rings[0] to use without a bounds check since we'd then have one mut ref inside transmit_rings and another ref for reading
-            // which is illegal, so... we use first_ring separately and copy the code
-            volatile::write(
-                &mut self.first_ring[self.process_delimiter as usize].metadata,
-                u64::to_le(self.outputs[0] as u64 | rs_bit | (1 << (24 + 1)) | (1 << 24)),
-            );
-            self.outputs[0] = 0;
-            let mut o: u8 = 1;
-            for r in &mut self.other_rings {
+            let mut o: u8 = 0;
+            for r in &mut self.rings {
                 volatile::write(
                     &mut r[self.process_delimiter as usize].metadata,
                     u64::to_le(self.outputs[o as usize] as u64 | rs_bit | (1 << (24 + 1)) | (1 << 24)),
