90c39f06a779131fe8eeba559defb782886b7229
Solal Pirelli <solal.pirelli@gmail.com>
2022-03-13 15:21:07 +0100

    Ada: finish buffer pool

--- ada/ixgbe_buffer_pool.adb
+++ ada/ixgbe_buffer_pool.adb
@@ -10,13 +10,13 @@ package body Ixgbe_Buffer_Pool is
   Fake_Buffer: aliased Buffer := (Data => Fake_Data'Access, Phys_Addr => 0, Length => 0);
 
   function Buffer_Pool_Allocate(Size: in UnsignedInteger) return Buffer_Pool is
-    type Buffer_Range is new UnsignedInteger range 0 .. Size;
+    type Buffer_Range is new UnsignedInteger range 0 .. Size - 1;
 
     type Buffer_Array is array(Buffer_Range) of aliased Buffer;
     function Buffer_Allocate is new Environment.Allocate(T => Buffer, R => Buffer_Range, T_Array => Buffer_Array);
     Buffers: not null access Buffer_Array := Buffer_Allocate;
 
-    Buffer_Accesses: Buffer_Access_Array(0 .. Size) := (others => Fake_Buffer'Access);
+    Buffer_Accesses: Buffer_Access_Array(0 .. Size - 1) := (others => Fake_Buffer'Access);
 
     type Data_Array is array(Buffer_Range) of aliased Packet_Data;
     function Data_Allocate is new Environment.Allocate(T => Packet_Data, R => Buffer_Range, T_Array => Data_Array);
@@ -27,16 +27,34 @@ package body Ixgbe_Buffer_Pool is
       Buffers(N) := (Data => Data(N)'Access, Phys_Addr => Get_Data_Address(Data(N)'Access), Length => 0);
       Buffer_Accesses(UnsignedInteger(N)) := Buffers(N)'Access;
     end loop;
-    return (Size => Size, Buffers => Buffer_Accesses, Index => (Size - 1));
+    return (Max => Size - 1, Buffers => Buffer_Accesses, Index => (Size - 2));
   end;
+
+  -- Note the <= Max, since Max is inclusive here!
 
   function Buffer_Pool_Give(Pool: in out Buffer_Pool; Buf: not null access Buffer) return Boolean is
   begin
-    null;
+    Pool.Index := Pool.Index + 1;
+    if Pool.Index <= Pool.Max then
+      Pool.Buffers(Pool.Index) := Buffer_Access(Buf);
+      return true;
+    end if;
+
+    Pool.Index := Pool.Index - 1;
+    return false;
   end;
 
   function Buffer_Pool_Take(Pool: in out Buffer_Pool) return access Buffer is
   begin
-    null;
+    if Pool.Index <= Pool.Max then
+      declare
+        Buf: not null access Buffer := Pool.Buffers(Pool.Index);
+      begin
+        Pool.Index := Pool.Index - 1;
+        return Buf;
+      end;
+    end if;
+
+    return null;
   end;
 end Ixgbe_Buffer_Pool;
--- ada/ixgbe_buffer_pool.ads
+++ ada/ixgbe_buffer_pool.ads
@@ -15,13 +15,13 @@ package Ixgbe_Buffer_Pool is
   type Buffer_Access is not null access all Buffer;
   type Buffer_Access_Array is array(UnsignedInteger range <>) of Buffer_Access;
 
-  -- NOTE: 'Size' here is one less than the desired side; cannot use 'Size-1' for the array ("discriminant in constraint must appear alone")
-  type Buffer_Pool(Size: UnsignedInteger) is record
-    Buffers: Buffer_Access_Array(0 .. Size);
+  -- NOTE: 'Max' here is one less than the desired side; cannot use 'Size-1' for the array ("discriminant in constraint must appear alone")
+  type Buffer_Pool(Max: UnsignedInteger) is record
+    Buffers: Buffer_Access_Array(0 .. Max);
     Index: UnsignedInteger;
   end record;
 
   function Buffer_Pool_Allocate(Size: in UnsignedInteger) return Buffer_Pool;
-  function Buffer_Pool_Give(Pool: in out Buffer_Pool; Buf: not null access Buffer) return Boolean;
-  function Buffer_Pool_Take(Pool: in out Buffer_Pool) return access Buffer;
+  function Buffer_Pool_Give(Pool: in out Buffer_Pool; Buf: not null access Buffer) return Boolean with Inline_Always;
+  function Buffer_Pool_Take(Pool: in out Buffer_Pool) return access Buffer with Inline_Always;
 end Ixgbe_Buffer_Pool;
