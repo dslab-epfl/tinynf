681133dd03d4fefa9bc25ed36b1a24c00819f648
Solal Pirelli <solal.pirelli@gmail.com>
2022-03-13 22:47:49 +0100

    Ada: Add queues for the other direction

--- ada/nf_queues.adb
+++ ada/nf_queues.adb
@@ -14,14 +14,14 @@ package body NF_Queues is
     package Rx is new Ixgbe_Queues_Rx(Size => Batch_Size);
     use Rx;
     Batch: Rx.B := (others => Fake_Buffer'Access);
-    N_Rx: Rx.R_Full := 0;
     Nb_Rx: Rx.R_Full;
     Discard: Boolean;
   begin
     loop
+      declare
+        N_Rx: Rx.R_Full := 0;
       begin
         Nb_Rx := Rx.Rx_Batch(Rx0, Batch);
---        Nb_Rx := Rx.R_Full'Last;
         while N_Rx < Nb_Rx loop
           NF.Handle_Data(Batch(Rx.R(N_Rx)).Data);
           N_Rx := N_Rx + 1;
@@ -38,6 +38,28 @@ package body NF_Queues is
             Nb_Tx := Tx.Tx_Batch(Tx1, Tx.B(Batch(0 .. Rx.R(Nb_Rx-1))));
             while Nb_Tx < UnsignedInteger(Nb_Rx) loop
               Discard := Buffer_Pool_Give(Tx1.Pool, Batch(Rx.R(Nb_Tx)));
+              Nb_Tx := Nb_Tx + 1;
+            end loop;
+          end if;
+        end;
+      end;
+      declare
+        N_Rx: Rx.R_Full := 0;
+      begin
+        Nb_Rx := Rx.Rx_Batch(Rx1, Batch);
+        while N_Rx < Nb_Rx loop
+          NF.Handle_Data(Batch(Rx.R(N_Rx)).Data);
+          N_Rx := N_Rx + 1;
+        end loop;
+        declare
+          package Tx is new Ixgbe_Queues_Tx(Size => UnsignedInteger(Nb_Rx));
+          use Tx;
+          Nb_Tx: UnsignedInteger;
+        begin
+          if Nb_Rx > 0 then
+            Nb_Tx := Tx.Tx_Batch(Tx0, Tx.B(Batch(0 .. Rx.R(Nb_Rx-1))));
+            while Nb_Tx < UnsignedInteger(Nb_Rx) loop
+              Discard := Buffer_Pool_Give(Tx0.Pool, Batch(Rx.R(Nb_Tx)));
               Nb_Tx := Nb_Tx + 1;
             end loop;
           end if;
