db25d73bc78178e28c2f19eff55c3de556b792b4
Solal Pirelli <solal.pirelli@gmail.com>
2022-03-15 16:49:12 +0100

    Experiments: finalize the table generation for metrics

--- experiments/code-metrics/tabulate-metrics.sh
+++ experiments/code-metrics/tabulate-metrics.sh
@@ -31,6 +31,7 @@ loc_ada="$(loc_count '--include-lang=Ada --not-match-f=ixgbe_agent_const ada')"
 # Prints the count of assembly instructions
 asm_count()
 {
+  # skip 1st and last line, which state beginning/end of dump
   gdb -batch -ex 'set disassembly-flavor intel' -ex "disassemble $1" "$2" | tail -n+2 | head -n-1 | wc -l
 }
 
@@ -49,11 +50,24 @@ if ! TN_MODE=2 make -f Makefile.benchmarking -C rust >/dev/null 2>&1 ; then exit
 run_queues_symbol="$(nm rust/target/release/tinynf | grep run | grep queues | grep tinynf | cut -d ' ' -f 3)"
 asm_rust_queues="$(asm_count "$run_queues_symbol" 'rust/target/release/tinynf')"
 
+# CSharp always has both compiled so we only compile once
+# There's also like 3 asm lines for the lambda initializing the default value of the buffer references for queues; it does not matter at this scale
+if ! TN_CSHARP_AOT=y make -f Makefile.benchmarking -C csharp >/dev/null 2>&1 ; then exit $? ; fi
+asm_csharp="$(asm_count 'TinyNF_TinyNF_Program__Run' csharp/TinyNF/bin/Release/*/*/publish/TinyNF)"
+asm_csharp_queues="$(asm_count 'TinyNF_TinyNF_Program__RunQueues' csharp/TinyNF/bin/Release/*/*/publish/TinyNF)"
+
+# Ada also has both
+if ! make -f Makefile.benchmarking -C ada >/dev/null 2>&1 ; then exit $? ; fi
+asm_ada="$(asm_count 'nf__run' 'ada/tinynf')"
+asm_ada_queues="$(asm_count 'nf_queues__run' 'ada/tinynf')"
+
+printf '\t\tasm\n'
+printf 'Lang\tLoC\trestr.\tflex.\n'
 
 printf 'C\t%s\t%s\t%s\n' "$loc_c" "$asm_c" "$asm_c_queues"
 printf 'Rust\t%s\t%s\t%s\n' "$loc_rust" "$asm_rust" "$asm_rust_queues"
-printf 'C#\t%s\n' "$loc_csharp"
-printf 'Ada\t%s\n' "$loc_ada"
+printf 'C#\t%s\t%s\t%s\n' "$loc_csharp" "$asm_csharp" "$asm_csharp_queues"
+printf 'Ada\t%s\t%s\t%s\n' "$loc_ada" "$asm_ada" "$asm_ada_queues"
 
 printf '\n'
 printf 'Rust extensions: %s lines of code\n' "$loc_rust_ext"
