c1265aca9db4ec33b0da5de9a5c74e773e409265
Solal Pirelli <solal.pirelli@gmail.com>
2023-01-18 11:39:32 +0100

    C#: Remove obsolete TODO

--- csharp/TinyNF.Environment/IEnvironment.cs
+++ csharp/TinyNF.Environment/IEnvironment.cs
@@ -5,7 +5,6 @@ namespace TinyNF.Environment;
 public interface IEnvironment
 {
     // Memory
-    // TODO this is dangerous, we need an initializer parameter
     Memory<T> Allocate<T>(int count) where T : unmanaged; // The semantics of this are rather silly: the resulting Memory<T> has an infinite lifetime!
     Memory<T> MapPhysicalMemory<T>(ulong addr, int count) where T : unmanaged; // addr is ulong, not nuint, because PCI BARs are 64-bit
     nuint GetPhysicalAddress<T>(ref T value);
