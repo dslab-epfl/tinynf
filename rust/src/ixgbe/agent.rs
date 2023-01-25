ba1177c1c407f41d6dce2d4af6e4c176a5b0e137
Solal Pirelli <solal.pirelli@gmail.com>
2023-01-25 11:14:36 +0100

    Rust: harmonize struct/field names with C

--- rust/src/ixgbe/agent.rs
+++ rust/src/ixgbe/agent.rs
@@ -10,13 +10,13 @@ const RECYCLE_PERIOD: u8 = 64;
 
 // OVERHEAD: each of these slices has the same length, but we can't state it in code
 pub struct Agent<'a> {
-    packets: &'a mut [PacketData; RING_SIZE],
+    buffers: &'a mut [PacketData; RING_SIZE],
     rings: LifedSlice<'a, LifedArray<'a, Descriptor, { RING_SIZE }>>,
-    receive_tail: LifedPtr<'a, u32>,
+    receive_tail_addr: LifedPtr<'a, u32>,
     transmit_heads: LifedSlice<'a, TransmitHead>,
-    transmit_tails: LifedSlice<'a, LifedPtr<'a, u32>>,
+    transmit_tail_addrs: LifedSlice<'a, LifedPtr<'a, u32>>,
     outputs: &'a mut [u64; MAX_OUTPUTS],
-    process_delimiter: u8,
+    processed_delimiter: u8,
 }
 
 impl<'a> Agent<'a> {
@@ -29,35 +29,35 @@ impl<'a> Agent<'a> {
             panic!("Too many outputs");
         }
 
-        let packets = env.allocate::<PacketData, { RING_SIZE }>();
+        let buffers = env.allocate::<PacketData, { RING_SIZE }>();
 
         let rings = env.allocate_slice::<LifedArray<'a, Descriptor, { RING_SIZE }>>(outputs.len());
         for r in 0..outputs.len() {
             rings[r] = LifedArray::new(env.allocate::<Descriptor, { RING_SIZE }>());
         }
-        for n in 0..packets.len() {
-            let packet_phys_addr = u64::to_le(env.get_physical_address(&mut packets[n as usize]) as u64);
+        for n in 0..buffers.len() {
+            let packet_phys_addr = u64::to_le(env.get_physical_address(&mut buffers[n as usize]) as u64);
             for r in 0..outputs.len() {
                 rings[r].index(n as usize).write_volatile_part(packet_phys_addr, |d| &mut d.addr);
             }
         }
 
-        let receive_tail = input.set_input(env, rings[0].index(0));
+        let receive_tail_addr = input.set_input(env, rings[0].index(0));
 
         let transmit_heads = LifedSlice::new(env.allocate_slice::<TransmitHead>(outputs.len()));
-        let transmit_tails = LifedSlice::new(env.allocate_slice::<LifedPtr<'a, u32>>(outputs.len()));
+        let transmit_tail_addrs = LifedSlice::new(env.allocate_slice::<LifedPtr<'a, u32>>(outputs.len()));
         for n in 0..outputs.len() {
-            transmit_tails.set(n, outputs[n].add_output(env, rings[n].index(0), transmit_heads.index(n)));
+            transmit_tail_addrs.set(n, outputs[n].add_output(env, rings[n].index(0), transmit_heads.index(n)));
         }
 
         Agent {
-            packets,
+            buffers,
             rings: LifedSlice::new(rings),
-            receive_tail,
+            receive_tail_addr,
             transmit_heads,
-            transmit_tails,
+            transmit_tail_addrs,
             outputs: env.allocate::<u64, { MAX_OUTPUTS }>(),
-            process_delimiter: 0,
+            processed_delimiter: 0,
         }
     }
 
@@ -65,46 +65,46 @@ impl<'a> Agent<'a> {
     pub fn run(&mut self, processor: fn(&mut PacketData, u64, &mut [u64; MAX_OUTPUTS])) {
         let mut n: u8 = 0;
         while n < FLUSH_PERIOD {
-            let receive_metadata = u64::from_le(self.rings.first().index(self.process_delimiter as usize).read_volatile_part(|d| &d.metadata));
+            let receive_metadata = u64::from_le(self.rings.first().index(self.processed_delimiter as usize).read_volatile_part(|d| &d.metadata));
             if (receive_metadata & RX_METADATA_DD) == 0 {
                 break;
             }
 
             let length = RX_METADATA_LENGTH(receive_metadata);
-            processor(&mut self.packets[self.process_delimiter as usize], length, self.outputs);
+            processor(&mut self.buffers[self.processed_delimiter as usize], length, self.outputs);
 
-            let rs_bit: u64 = if self.process_delimiter % RECYCLE_PERIOD == (RECYCLE_PERIOD - 1) { TX_METADATA_RS } else { 0 };
+            let rs_bit: u64 = if self.processed_delimiter % RECYCLE_PERIOD == (RECYCLE_PERIOD - 1) { TX_METADATA_RS } else { 0 };
             for o in 0..self.rings.len() {
                 self.rings
                     .get(o as usize)
-                    .index(self.process_delimiter as usize)
+                    .index(self.processed_delimiter as usize)
                     .write_volatile_part(u64::to_le(TX_METADATA_LENGTH(self.outputs[o as u8 as usize]) | rs_bit | TX_METADATA_IFCS | TX_METADATA_EOP), |d| {
                         &mut d.metadata
                     });
                 self.outputs[o as u8 as usize] = 0;
             }
 
-            self.process_delimiter = self.process_delimiter.wrapping_add(1); // modulo ring size implicit since it's an u8
+            self.processed_delimiter = self.processed_delimiter.wrapping_add(1); // modulo ring size implicit since it's an u8
 
             if rs_bit != 0 {
-                let mut earliest_transmit_head = self.process_delimiter as u32;
+                let mut earliest_transmit_head = self.processed_delimiter as u32;
                 let mut min_diff = u64::MAX;
                 for h in 0..self.transmit_heads.len() {
                     let head_value = u32::from_le(self.transmit_heads.index(h).read_volatile_part(|h| &h.value));
-                    let diff = (head_value as u64).wrapping_sub(self.process_delimiter as u64);
+                    let diff = (head_value as u64).wrapping_sub(self.processed_delimiter as u64);
                     if diff <= min_diff {
                         earliest_transmit_head = head_value;
                         min_diff = diff;
                     }
                 }
 
-                self.receive_tail.write_volatile(u32::to_le(earliest_transmit_head));
+                self.receive_tail_addr.write_volatile(u32::to_le(earliest_transmit_head));
             }
             n += 1;
         }
         if n != 0 {
-            for t in 0..self.transmit_tails.len() {
-                self.transmit_tails.get(t).write_volatile(u32::to_le(self.process_delimiter as u32));
+            for t in 0..self.transmit_tail_addrs.len() {
+                self.transmit_tail_addrs.get(t).write_volatile(u32::to_le(self.processed_delimiter as u32));
             }
         }
     }
--- rust/src/ixgbe/agent_const.rs
+++ rust/src/ixgbe/agent_const.rs
@@ -7,48 +7,48 @@ const FLUSH_PERIOD: u8 = 8;
 const RECYCLE_PERIOD: u8 = 64;
 
 pub struct AgentConst<'a, const OUTPUTS: usize> {
-    packets: &'a mut [PacketData; RING_SIZE],
-    receive_tail: LifedPtr<'a, u32>,
+    buffers: &'a mut [PacketData; RING_SIZE],
     rings: &'a mut [LifedArray<'a, Descriptor, { RING_SIZE }>; OUTPUTS],
+    receive_tail_addr: LifedPtr<'a, u32>,
     transmit_heads: LifedArray<'a, TransmitHead, { OUTPUTS }>,
-    transmit_tails: &'a mut [LifedPtr<'a, u32>; OUTPUTS],
+    transmit_tail_addrs: &'a mut [LifedPtr<'a, u32>; OUTPUTS],
     outputs: &'a mut [u64; OUTPUTS],
-    process_delimiter: u8,
+    processed_delimiter: u8,
 }
 
 impl<'a, const OUTPUTS: usize> AgentConst<'a, OUTPUTS> {
     pub fn create(env: &impl Environment<'a>, input: &Device<'a>, outputs: [&Device<'a>; OUTPUTS]) -> AgentConst<'a, OUTPUTS> {
-        let packets = env.allocate::<PacketData, { RING_SIZE }>();
+        let buffers = env.allocate::<PacketData, { RING_SIZE }>();
 
         let rings = env.allocate::<LifedArray<'a, Descriptor, { RING_SIZE }>, { OUTPUTS }>();
         for r in 0..rings.len() {
             rings[r] = LifedArray::new(env.allocate::<Descriptor, { RING_SIZE }>());
         }
         for n in 0..RING_SIZE {
-            let packet_phys_addr = u64::to_le(env.get_physical_address(&mut packets[n as usize]) as u64);
+            let packet_phys_addr = u64::to_le(env.get_physical_address(&mut buffers[n as usize]) as u64);
             for r in 0..OUTPUTS {
                 rings[r].index(n as usize).write_volatile_part(packet_phys_addr, |d| &mut d.addr);
             }
         }
 
-        let receive_tail = input.set_input(env, rings[0].index(0));
+        let receive_tail_addr = input.set_input(env, rings[0].index(0));
 
         let transmit_heads = LifedArray::new(env.allocate::<TransmitHead, { OUTPUTS }>());
-        let transmit_tails = env.allocate::<LifedPtr<'a, u32>, { OUTPUTS }>();
+        let transmit_tail_addrs = env.allocate::<LifedPtr<'a, u32>, { OUTPUTS }>();
         let mut t = 0;
         for out in outputs {
-            transmit_tails[t] = out.add_output(env, rings[t].index(0), transmit_heads.index(t));
+            transmit_tail_addrs[t] = out.add_output(env, rings[t].index(0), transmit_heads.index(t));
             t = t + 1;
         }
 
         AgentConst {
-            packets,
-            receive_tail,
+            buffers,
+            receive_tail_addr,
             rings,
             transmit_heads,
-            transmit_tails,
+            transmit_tail_addrs,
             outputs: env.allocate::<u64, { OUTPUTS }>(),
-            process_delimiter: 0,
+            processed_delimiter: 0,
         }
     }
 
@@ -56,43 +56,43 @@ impl<'a, const OUTPUTS: usize> AgentConst<'a, OUTPUTS> {
     pub fn run(&mut self, processor: fn(&mut PacketData, u64, &mut [u64; OUTPUTS])) {
         let mut n: u8 = 0;
         while n < FLUSH_PERIOD {
-            let receive_metadata = u64::from_le(self.rings[0].index(self.process_delimiter as usize).read_volatile_part(|d| &d.metadata));
+            let receive_metadata = u64::from_le(self.rings[0].index(self.processed_delimiter as usize).read_volatile_part(|d| &d.metadata));
             if (receive_metadata & RX_METADATA_DD) == 0 {
                 break;
             }
 
             let length = RX_METADATA_LENGTH(receive_metadata);
-            processor(&mut self.packets[self.process_delimiter as usize], length, self.outputs);
+            processor(&mut self.buffers[self.processed_delimiter as usize], length, self.outputs);
 
-            let rs_bit: u64 = if self.process_delimiter % RECYCLE_PERIOD == (RECYCLE_PERIOD - 1) { TX_METADATA_RS } else { 0 };
+            let rs_bit: u64 = if self.processed_delimiter % RECYCLE_PERIOD == (RECYCLE_PERIOD - 1) { TX_METADATA_RS } else { 0 };
             for o in 0..OUTPUTS {
                 self.rings[o]
-                    .index(self.process_delimiter as usize)
+                    .index(self.processed_delimiter as usize)
                     .write_volatile_part(u64::to_le(TX_METADATA_LENGTH(self.outputs[o]) | rs_bit | TX_METADATA_IFCS | TX_METADATA_EOP), |d| &mut d.metadata);
                 self.outputs[o] = 0;
             }
 
-            self.process_delimiter += 1;
+            self.processed_delimiter += 1;
 
             if rs_bit != 0 {
-                let mut earliest_transmit_head = self.process_delimiter as u32;
+                let mut earliest_transmit_head = self.processed_delimiter as u32;
                 let mut min_diff = u64::MAX;
                 for h in 0..OUTPUTS {
                     let head_value = u32::from_le(self.transmit_heads.index(h).read_volatile_part(|h| &h.value));
-                    let diff = (head_value as u64).wrapping_sub(self.process_delimiter as u64);
+                    let diff = (head_value as u64).wrapping_sub(self.processed_delimiter as u64);
                     if diff <= min_diff {
                         earliest_transmit_head = head_value;
                         min_diff = diff;
                     }
                 }
 
-                self.receive_tail.write_volatile(u32::to_le(earliest_transmit_head.wrapping_sub(1) % RING_SIZE as u32));
+                self.receive_tail_addr.write_volatile(u32::to_le(earliest_transmit_head.wrapping_sub(1) % RING_SIZE as u32));
             }
             n += 1;
         }
         if n != 0 {
-            for tail in self.transmit_tails.into_iter() {
-                tail.write_volatile(u32::to_le(self.process_delimiter as u32));
+            for tail in self.transmit_tail_addrs.into_iter() {
+                tail.write_volatile(u32::to_le(self.processed_delimiter as u32));
             }
         }
     }
