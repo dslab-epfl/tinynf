3f1f42b030e85a606818c633271b76aa22dba957
Solal Pirelli <solal.pirelli@gmail.com>
2023-01-25 14:53:47 +0100

    C#: Restore working Ref.cs

--- csharp/TinyNF.Unsafe/Ref.cs
+++ csharp/TinyNF.Unsafe/Ref.cs
@@ -6,30 +6,31 @@ namespace TinyNF.Unsafe;
 /// A reference to a value, i.e., a way to get a 'ref T' field.
 /// This struct is safe iff:
 /// - it is constructed using an explicit constructor, not the default one; and
-/// - its IntPtr-based constructor is only used from a .Ptr of an existing Ref
+/// - its IntPtr-based constructor is only used from a .Ptr of an existing Ref; and
+/// - it is constructed from a (ref or IntPtr) to a pinned value
 /// </summary>
 /// <remarks>
-/// The IntPtr part is to allow storing a Ref as a non-ref-struct, which is horribly hacky
-/// but needed for arrays of ref to ref structs (see e.g. the buffer pool).
+/// This exists to allow storing a Ref as a non-ref-struct, which is horribly hacky
+/// but needed for arrays of ref to ref structs (see e.g. the buffer pool)
 /// </remarks>
 public readonly unsafe ref struct Ref<T>
 {
-    private readonly ref T _value;
+    private readonly IntPtr _value;
 
-    public IntPtr Ptr => (IntPtr)System.Runtime.CompilerServices.Unsafe.AsPointer(ref _value);
+    public IntPtr Ptr => (IntPtr)_value;
 
     public Ref(ref T value)
     {
-        _value = ref value;
+        _value = (IntPtr)System.Runtime.CompilerServices.Unsafe.AsPointer(ref value);
     }
 
     public Ref(IntPtr value)
     {
-        _value = System.Runtime.CompilerServices.Unsafe.AsRef<T>((void*)value);
+        _value = value;
     }
 
     public readonly ref T Get()
     {
-        return ref _value;
+        return ref System.Runtime.CompilerServices.Unsafe.AsRef<T>((void*)_value);
     }
 }
